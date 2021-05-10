5c16696a8f0790a62d0dbdc69110abb5cd1c89f7
paulapasite <79280336+paulapasite@users.noreply.github.com>
2021-05-10 09:12:46 +0300

    Add files via upload

---
+++ bmi calculator/bmi calculator/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace bmi_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Weight = new System.Windows.Forms.TextBox();
+            this.Height = new System.Windows.Forms.TextBox();
+            this.BMI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(53, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULATE BMI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(336, 423);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 40);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "RESET BMI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(583, 423);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 40);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(53, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(610, 118);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BMI CALCULATOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(53, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Your Weight";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(53, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 48);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Enter Your Height";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(53, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 48);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Your BMI is";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Poppins", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(583, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 48);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "KG";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(583, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 48);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "CM";
+            this.label7.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Weight
+            // 
+            this.Weight.Location = new System.Drawing.Point(336, 155);
+            this.Weight.Multiline = true;
+            this.Weight.Name = "Weight";
+            this.Weight.Size = new System.Drawing.Size(218, 48);
+            this.Weight.TabIndex = 2;
+            this.Weight.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Height
+            // 
+            this.Height.Location = new System.Drawing.Point(336, 222);
+            this.Height.Multiline = true;
+            this.Height.Name = "Height";
+            this.Height.Size = new System.Drawing.Size(218, 48);
+            this.Height.TabIndex = 2;
+            this.Height.TextChanged += new System.EventHandler(this.Height_TextChanged);
+            // 
+            // BMI
+            // 
+            this.BMI.Location = new System.Drawing.Point(336, 282);
+            this.BMI.Multiline = true;
+            this.BMI.Name = "BMI";
+            this.BMI.Size = new System.Drawing.Size(218, 48);
+            this.BMI.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 531);
+            this.Controls.Add(this.BMI);
+            this.Controls.Add(this.Height);
+            this.Controls.Add(this.Weight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Weight;
+        private System.Windows.Forms.TextBox Height;
+        private System.Windows.Forms.TextBox BMI;
+    }
+}
+
---
+++ bmi calculator/bmi calculator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmi_calculator
+{
+    public partial class Form1 : Form
+    {
+        double h;
+        double w;
+        double t;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+          
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BMI.BackColor = Color.White;
+
+            h = Convert.ToDouble(Height.Text);
+            w = Convert.ToDouble(Weight.Text);
+            t = (w/h/h)*10000;
+            BMI.Text = t.ToString("F2");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            Height.Clear();
+            Weight.Clear();
+            BMI.Text = "";
+            BMI.BackColor = Color.White;
+        }
+
+
+        private void Height_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
